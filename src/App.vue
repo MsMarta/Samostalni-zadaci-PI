ddf0c85e863c0a2e63b95ee9ec7103449fde0e4c
MsMarta <98046290+MsMarta@users.noreply.github.com>
2025-03-25 23:27:13 +0100

    Samostalni zadatak 1

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+
+<script setup>
+
+</script>
+
+<template>
+<div class="min-h-screen bg-gradient-to-br from-emerald-950 to-indigo-950 text-teal-100 flex flex-col items-center p-4 sm:p-8">
+
+    <h1 class="text-[(60)px] sm:text-4xl font-bold text-center">
+      Pozdrav, ja sam <span class="text-cyan-400">Marta Kralj</span>
+    </h1>
+    
+    <div class="w-3/4 sm:w-1/2 h-0.5 bg-teal-100/75 my-4"></div>
+
+    <p class="text-base sm:text-lg mb-6 text-center text-cyan-100/75">Moji omiljeni filmovi:</p>
+
+    <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 sm:gap-6">
+      <div class="flex flex-col items-center">
+        <img class="w-40 h-56 sm:w-48 sm:h-64 rounded-lg object-cover" src="https://i.guim.co.uk/img/media/6b84d191353d5f505a76c4e2bfa4caed5387d7d2/102_0_1844_1107/master/1844.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=6b589d1a632c3af3523294eb45fd7ea2" >
+            
+        </img>
+        <p class="mt-2 text-sm text-center">Garfield</p>
+      </div>
+
+      <div class="flex flex-col items-center">
+        <img class="w-40 h-56 sm:w-48 sm:h-64 rounded-lg object-cover" src="https://pixel.disco.skyshowtime.com/uuid/bbdf9f1d-dbb1-32f2-8564-7f55566ec6ce/COVER_TITLE_TALL?language=hr-HR&proposition=SKYSHOWTIME&version=978d188e-fc6b-34e0-9fed-779487561993">
+         
+        </img>
+        <p class="mt-2 text-sm text-center">Shrek</p>
+      </div>
+      <div class="flex flex-col items-center">
+        <img class="w-40 h-56 sm:w-48 sm:h-64 rounded-lg object-cover" src="https://resizing.flixster.com/Ybk529MeubLjsXf7vpebKjluieI=/fit-in/352x330/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p34876_v_v8_as.jpg">
+          
+        </img>
+        <p class="mt-2 text-sm text-center">Riba ribi grize rep</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
